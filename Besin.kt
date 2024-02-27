85242a30f0ab0c0b1e73d872e98a715f0f74ae03
myavuz76 <62217326+myavuz76@users.noreply.github.com>
2024-02-27 02:18:35 +0100

    Create Besin.kt

---
+++ Besin.kt
@@ -0,0 +1,33 @@
+package com.example.besinleruygulamasi.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
+
+//@Entity room veri tabanindan veri cekilirken kullanilir
+
+@Entity
+data class Besin(
+    @ColumnInfo("isim")
+    @SerializedName("isim")
+    val besinIsmi:String?,
+    @ColumnInfo("kalori")
+    @SerializedName("kalori")
+    val besinKalori:String?,
+    @ColumnInfo("karbonhidrat")
+    @SerializedName("karbonhidrat")
+    val besinKarbonhidrat : String?,
+    @ColumnInfo("protein")
+    @SerializedName("protein")
+    val besinProtein:String?,
+    @ColumnInfo("yag")
+    @SerializedName("yag")
+    val besinYag: String?,
+    @ColumnInfo("gorsel")
+    @SerializedName("gorsel")
+    val besinGorsel: String?
+) {
+    @PrimaryKey(autoGenerate = true)
+    var uuid :Int =0
+}
